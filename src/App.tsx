2675385a006311b7c4007f86987fc34c9b82c470
RiseMeUp <zenek7@mail.ru>
2022-05-22 21:37:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import './Technologies'
+import Technologies from "./Technologies";
+import Header from "./Header";
+
+function App() {
+    debugger
+    return (
+        <div>
+        <Header/>
+          <Technologies/>
+       </div>
+    );
+}
+
+
+
+
+export default App;
