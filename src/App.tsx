6741df12c959c1a0f14283aeaf1789ccdf2d0fa0
RiseMeUp <zenek7@mail.ru>
2022-05-22 22:10:32 +0300

    kabzda lesson 1 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import './App.css';
-import './Technologies'
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 function App() {
     debugger
     return (
         <div>
-        <Header/>
-          <Technologies/>
-       </div>
+            This is APP component!
+            <Rating/>
+            <Accordion/>
+        </div>
     );
 }
 
+function Accordion() {
+    debugger
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
 
 
+        </div>
+
+    );
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <div>Start</div>
+            <div>Start</div>
+            <div>Start</div>
+            <div>Start</div>
+            <div>Start</div>
+        </div>
+    );
+
+}
 
 export default App;
